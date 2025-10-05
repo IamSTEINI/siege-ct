6072fb6a28df3cf34def88b2affed350e41d06f8
Stein <86646466+IamSTEINI@users.noreply.github.com>
2025-10-05 12:04:29 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
 export default function Home() {
-  return (
-   <div></div>
-  );
+	return (
+		<div className="flex-col flex absolute justify-center items-center w-screen h-screen">
+			<div className="w-5/12 flex-row items-center justify-center bg-zinc-900 h-28 border-gray-700 border rounded-md">
+				<span className="w-full font-bold text-center text-xl tracking-wider h-full flex flex-col items-center justify-center px-5 text-white">
+					🏗️🚧 Wait till next siege week! Web version is in work.
+					<pre className="opacity-50">- Stein</pre>
+				</span>
+			</div>
+		</div>
+	);
 }
